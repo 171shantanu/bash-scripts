767ef593b9f71ae80a78111274092fac71dd3ee3
shantanu <shantanu171@gmail.com>
2023-04-10 13:26:20 +0530

    Updated universal update script

--- universal-update.sh
+++ universal-update.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# Usally copy this system at the path /usr/local/bin if you are running it local.
+# Keep the access of the script to root/admin users only.
+
 release_file=/etc/os-release
 logfile=/var/log/updater.log
 errorlog=/var/log/updater_errors.log 
+
+check_exit_status() {
+    if [ $? -ne 0 ]
+    then
+        echo "There is an error, please check the $errorlog file."
+}
 
 # if satement that will run for Arch based distribution.
 if grep -q "Arch" $release_file
 then
     #If the host is based on Arch it will run the pacman update command
     sudo pacman -Syu 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0 ]
-    then
-        echo "There is an error, please check the $errorlog file."
-    file
+    check_exit_status
 fi 
 
 # if satement that will run for Debian/Ubuntu based distribution.
@@ -20,13 +26,8 @@ if grep -q "Debian" $release_file || if grep -q "Ubuntu" $release_file
 then
     #If the host is based on Debain/Ubuntu it will run the apt update command
     sudo apt update 1>>$logfile 2>>$errorlog
-    if [ $? ne 0 ]
-    then
-        echo "There is an error, please check the $errorlog file."
-    fi 
+    check_exit_status
+    
     sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
-    if [ $? ne 0 ]
-    then
-        echo "There is an error, please check the $errorlog file."
-    fi 
-fi  
+    check_exit_status
+fi   
