a138ab6b9a3a5fdd9515e6c54feb41b05518af02
shantanu <shantanu171@gmail.com>
2023-04-10 09:15:33 +0530

    Adhoc

--- copy-ssh-ansible.sh
+++ copy-ssh-ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # define the IP addresses of the servers
-SERVERS="192.168.10.15 192.168.10.18 192.168.10.65"
+SERVERS="54.160.161.164 35.171.225.179 3.89.85.20"
 
 # define the path to the SSH key on your local host which needs to be copied tothe servers
 LOCAL_KEY="/Users/shantanu/.ssh/ansible"
@@ -10,15 +10,15 @@ LOCAL_KEY="/Users/shantanu/.ssh/ansible"
 REMOTE_DIR="/home/ubuntu/.ssh"
 
 # define the name of the SSH key on the servers
-REMOTE_KEY="ansible"
+REMOTE_KEY="ansible.pub"
 
 # define the username to use for the SSH connection
 USER="ubuntu"
 
 # define the path to the private key to establish the SSH connection to the servers.
-PRIVATE_KEY="/path/to/private/key"
+PRIVATE_KEY="/Users/shantanu/Desktop/learning-repos/managed-k8s-tf/self-k8s-key.pem"
 
 # loop through each server and copy the SSH key
 for SERVER in $SERVERS; do
-    scp -i $PRIVATE_KEY $LOCAL_KEY $USER@$SERVER:$REMOTE_DIR/$REMOTE_KEY
+  scp -i $PRIVATE_KEY $LOCAL_KEY $USER@$SERVER:$REMOTE_DIR/$REMOTE_KEY
 done
