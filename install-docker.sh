575ab9c7ec01f427240f53b68530023b29bcfc39
shantanu <shantanu171@gmail.com>
2023-04-17 19:08:39 +0530

    Added script that installs docker on linux

---
+++ install-docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if command -v yum &> /dev/null; then
+  # Install Docker on CentOS/RedHat using yum
+  sudo yum update -y
+  sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  sudo yum install -y docker-ce docker-ce-cli containerd.io
+  sudo systemctl start docker
+  sudo systemctl enable docker
+elif command -v apt &> /dev/null; then
+  # Install Docker on Ubuntu/Debian using apt
+  sudo apt update
+  sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt update
+  sudo apt install -y docker-ce docker-ce-cli containerd.io
+  sudo systemctl start docker
+  sudo systemctl enable docker
+else
+  echo "Unknown package manager"
+fi
+    
